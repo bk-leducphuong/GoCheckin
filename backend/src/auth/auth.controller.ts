a76341a379bac7d82859bd546cf0b8b29bdb9d5f
Le Duc Phuong <phuongtroc2004@gmail.com>
2025-03-12 18:30:23 +0700

    docs: add swagger api docs

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,19 +1,33 @@
 import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { AuthLoginDto } from './dto/auth-login.dto';
 import { AuthRegisterDto } from './dto/auth-register.dto';
 import { AuthLoginResponseDto } from './dto/login-response.dto';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly service: AuthService) {}
 
+  @ApiOperation({ summary: 'User login' })
+  @ApiResponse({
+    status: 200,
+    description: 'Successful login',
+    type: AuthLoginResponseDto,
+  })
   @HttpCode(HttpStatus.OK)
   @Post('login')
   login(@Body() loginDto: AuthLoginDto): Promise<AuthLoginResponseDto> {
     return this.service.login(loginDto);
   }
 
+  @ApiOperation({ summary: 'User registration' })
+  @ApiResponse({
+    status: 201,
+    description: 'Successful registration',
+    type: AuthLoginResponseDto,
+  })
   @Post('register')
   register(
     @Body() registerDto: AuthRegisterDto,
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const appOptions = { cors: true };
   const app = await NestFactory.create(AppModule, appOptions);
   app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('GoCheckin')
+    .setDescription('The GoCheckin API Documents')
+    .setVersion('1.0')
+    .addTag('GoCheckin')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, documentFactory);
+
   const configService = app.get(ConfigService);
   const port: number = configService.get<number>('PORT') || 3000;
   await app.listen(port);
