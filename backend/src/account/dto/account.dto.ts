a39836af47343139b859a554e22aa59f1dc755ea
Le Duc Phuong <phuongtroc2004@gmail.com>
2025-03-12 18:08:57 +0700

    refactor(auth, account): add class-validator

--- backend/src/account/dto/account.dto.ts
+++ backend/src/account/dto/account.dto.ts
@@ -1,16 +1,51 @@
 import { UserRole } from '../entities/account.entity';
+import {
+  IsString,
+  IsEmail,
+  IsOptional,
+  IsBoolean,
+  IsEnum,
+  IsDate,
+} from 'class-validator';
 
 export class AccountDto {
+  @IsString()
   id: string;
+
+  @IsString()
   username: string;
+
+  @IsEmail()
   email: string;
+
+  @IsOptional()
+  @IsString()
   fullName?: string;
+
+  @IsOptional()
+  @IsString()
   phoneNumber?: string;
+
+  @IsBoolean()
   active: boolean;
+
+  @IsEnum(UserRole)
   role: UserRole;
+
+  @IsOptional()
+  @IsString()
   companyName?: string;
+
+  @IsOptional()
+  @IsDate()
   lastLogin?: Date;
+
+  @IsBoolean()
   enabled: boolean;
+
+  @IsDate()
   createdAt: Date;
+
+  @IsDate()
   updatedAt: Date;
 }
--- backend/src/account/dto/create-account.dto.ts
+++ backend/src/account/dto/create-account.dto.ts
@@ -1,13 +1,44 @@
 import { UserRole } from '../entities/account.entity';
+import {
+  IsString,
+  IsEmail,
+  IsOptional,
+  IsBoolean,
+  IsEnum,
+  MinLength,
+} from 'class-validator';
 
 export class CreateAccountDto {
+  @IsString()
+  @MinLength(2)
   username: string;
+
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @MinLength(6)
   password: string;
+
+  @IsString()
   fullName: string;
+
+  @IsOptional()
+  @IsString()
   phoneNumber?: string;
+
+  @IsEnum(UserRole)
   role: UserRole;
+
+  @IsOptional()
+  @IsString()
   companyName?: string;
+
+  @IsOptional()
+  @IsBoolean()
   active?: boolean;
+
+  @IsOptional()
+  @IsBoolean()
   enabled?: boolean;
 }
