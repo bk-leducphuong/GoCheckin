c76b8add83959b44bc907571ed8b574b6ecd078e
Le Duc Phuong <phuongtroc2004@gmail.com>
2025-03-18 18:59:55 +0700

    fix(account module): fix account module

--- backend/src/account/account.module.ts
+++ backend/src/account/account.module.ts
@@ -3,11 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AccountController } from './account.controller';
 import { AccountService } from './account.service';
 import { Account } from './entities/account.entity';
+import { AccountTenant } from './entities/account-tenant.entity';
+import { AccountTenantService } from './services/account-tenant.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Account])], // uses the forFeature() method to define which repositories are registered in the current scope
+  imports: [TypeOrmModule.forFeature([Account, AccountTenant])],
   controllers: [AccountController],
-  providers: [AccountService],
-  exports: [AccountService],
+  providers: [AccountService, AccountTenantService],
+  exports: [AccountService, AccountTenantService],
 })
 export class AccountModule {}
