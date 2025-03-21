13a0453c45933e64742554eedf454c885016077a
Le Duc Phuong <phuongtroc2004@gmail.com>
2025-03-21 10:58:03 +0700

    fix(jwt): fix small bug

--- backend/src/auth/strategies/jwt.strategy.ts
+++ backend/src/auth/strategies/jwt.strategy.ts
@@ -18,6 +18,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     // Return the payload which will be attached to request.user
     return {
       userId: payload.userId,
+      username: payload.username,
       email: payload.email,
       role: payload.role,
     };
