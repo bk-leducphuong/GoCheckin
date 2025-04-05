b678fb770b152a3f58591fc5111083fb80a1cbdf
Le Duc Phuong <phuongtroc2004@gmail.com>
2025-04-05 18:00:32 +0700

    fix: fix event creation function bug

--- backend/src/event/event.module.ts
+++ backend/src/event/event.module.ts
@@ -3,9 +3,10 @@ import { EventController } from './event.controller';
 import { EventService } from './event.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Event } from './entities/event.entity';
+import { AccountTenant } from 'src/account/entities/account-tenant.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Event])],
+  imports: [TypeOrmModule.forFeature([Event, AccountTenant])],
   controllers: [EventController],
   providers: [EventService],
   exports: [EventService],
